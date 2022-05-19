67a943a208896fdc8109b4361efc14e8b0efe05d
Craig Robson <craig.robson1@ncl.ac.uk>
2022-05-19 11:33:09 +0100

    Updates as mentioned in previous commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 import geopandas as gpd
 import pandas as pd
 import zipfile, io
-from os import listdir, getenv, mkdir
+from os import listdir, getenv, mkdir, remove
 import os
 from os.path import isfile, join, isdir
 
@@ -23,69 +23,87 @@ api_username, api_password = fetch_settings()
 user_settings['user'] = api_username
 user_settings['password'] = api_password
 
+# get list of area codes to run for - must be LAD codes
+area_code_list = getenv('area_codes')
+area_code_list = area_code_list.split(';')
+
 def mk_dir(path):
     """"""
     if isdir(path) is False:
         mkdir(path)
 
+def mk_dir_delete(path):
+    """"""
+    if isdir(path) is True:
+        files = [f for f in listdir(path) if isfile(join(path, f))]
+        for file in files:
+            remove(join(path, file))
+    else:
+        mkdir(path)
+
+    return
 
 mk_dir('/data')
-mk_dir('/data/downloads')
-mk_dir('/data/outputs')
-mk_dir('/data/outputs/developed')
-mk_dir('/data/outputs/developed_exroads')
-mk_dir('/data/outputs/final')
+mk_dir_delete('/data/downloads')
+mk_dir_delete('/data/outputs')
+mk_dir_delete('/data/outputs/developed')
+mk_dir_delete('/data/outputs/developed_exroads')
+mk_dir_delete('/data/outputs/final')
 out_dir = '/data/downloads'
 
-area_codes = 'W06000012,'
-queryText = f"https://www.nismod.ac.uk/api/data/boundaries/msoas_in_lad?export_format=geojson&area_codes={area_codes}"
-print(queryText)
-response = requests.get(queryText, auth=(user_settings['user'], user_settings['password']), verify=False)
-if response.status_code != 200:
-    print('API call for fetch MSOA list failed! Response: ', response.status_code)
-print(response.status_code)
-# load msoa/zone data into geodataframe
-area_zones = json.loads(response.text)
-gdf_zones = gpd.GeoDataFrame.from_features(area_zones["features"])
 
-zone_codes = list(gdf_zones['msoa_code'])
-
-
-## fetch topo polygons from NISMOD-DB
-scale = 'msoa'
-
-for zone_code in zone_codes:
-    print(zone_code)
-    queryText = f"https://www.nismod.ac.uk/api/data/mastermap/areas?export_format=geojson-zip&geom_format=geojson&scale={scale}&area_codes={zone_code}&year=2017&classification_codes=all&make=Manmade&flatten_lists=true"
+for area_codes in area_code_list:
+    if area_codes == '':
+        break
+    print(area_codes)
+    queryText = f"https://www.nismod.ac.uk/api/data/boundaries/msoas_in_lad?export_format=geojson&area_codes={area_codes}"
+    print(queryText)
     response = requests.get(queryText, auth=(user_settings['user'], user_settings['password']), verify=False)
+    if response.status_code != 200:
+        print('API call for fetch MSOA list failed! Response: ', response.status_code)
     print(response.status_code)
-    z = zipfile.ZipFile(io.BytesIO(response.content))
-    z.extractall(join(out_dir, area_codes[:-1]))
-    break
+    # load msoa/zone data into geodataframe
+    area_zones = json.loads(response.text)
+    gdf_zones = gpd.GeoDataFrame.from_features(area_zones["features"])
+
+    zone_codes = list(gdf_zones['msoa_code'])
 
 
-# get list of files downloaded from API
-data_files = [f for f in listdir(join(out_dir, area_codes[:-1])) if isfile(join(out_dir, area_codes[:-1], f))]
+    ## fetch topo polygons from NISMOD-DB
+    scale = 'msoa'
 
-# for testing only
-#gdf = gpd.read_file(join(out_dir, area_codes[:-1], data_files[1]))
-#gdf.head()
+    for zone_code in zone_codes:
+        print(zone_code)
+        queryText = f"https://www.nismod.ac.uk/api/data/mastermap/areas?export_format=geojson-zip&geom_format=geojson&scale={scale}&area_codes={zone_code}&year=2017&classification_codes=all&make=Manmade&flatten_lists=true"
+        response = requests.get(queryText, auth=(user_settings['user'], user_settings['password']), verify=False)
+        print(response.status_code)
+        z = zipfile.ZipFile(io.BytesIO(response.content))
+        z.extractall(join(out_dir, area_codes[:-1]))
+        break
 
-# loop through all downloaded geojson files and create a geodataframe
-path = [os.path.join(out_dir, area_codes[:-1], i) for i in data_files if ".geojson" in i]
-gdf = gpd.GeoDataFrame(pd.concat([gpd.read_file(i) for i in path],
-                                 ignore_index=True), crs=gpd.read_file(path[0]).crs)
 
-# drop duplicate polygons and filter for manmade/developed surfaces
-gdf = gdf.drop_duplicates()
-gdf = gdf.loc[gdf['make'].isin(['Manmade', 'Multiple'])]
+    # get list of files downloaded from API
+    data_files = [f for f in listdir(join(out_dir, area_codes[:-1])) if isfile(join(out_dir, area_codes[:-1], f))]
 
-# generate layer of urban/developed surfaces
-gdf.to_file('/data/outputs/developed/%s.gpkg' % area_codes[:-1], driver='GPKG')
+    # for testing only
+    #gdf = gpd.read_file(join(out_dir, area_codes[:-1], data_files[1]))
+    #gdf.head()
 
-# generate layer excluding toads
-gdf_nr = gdf[~gdf.theme.str.contains('Roads Tracks And Paths')]
-gdf_nr.to_file('/data/outputs/developed_exroads/%s.gpkg' % area_codes[:-1], driver='GPKG')
+    # loop through all downloaded geojson files and create a geodataframe
+    path = [os.path.join(out_dir, area_codes[:-1], i) for i in data_files if ".geojson" in i]
+    gdf = gpd.GeoDataFrame(pd.concat([gpd.read_file(i) for i in path],
+                                     ignore_index=True), crs=gpd.read_file(path[0]).crs)
+
+    # drop duplicate polygons and filter for manmade/developed surfaces
+    gdf = gdf.drop_duplicates()
+    gdf = gdf.loc[gdf['make'].isin(['Manmade', 'Multiple'])]
+
+    # generate layer of urban/developed surfaces
+    gdf.to_file('/data/outputs/developed/%s.gpkg' % area_codes, driver='GPKG')
+
+    # generate layer excluding toads
+    gdf_nr = gdf[~gdf.theme.str.contains('Roads Tracks And Paths')]
+    gdf_nr.to_file('/data/outputs/developed_exroads/%s.gpkg' % area_codes, driver='GPKG')
 
 # loop through all the generated files and create a geodataframe
 data_files = [f for f in listdir(join('/data/outputs/developed_exroads')) if isfile(join('/data/outputs/developed_exroads', f))]
